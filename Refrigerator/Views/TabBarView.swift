ee6ceedd4492ca5a4d412bf94c6caee139a4a24a
Ryan <ryandu9221@gmail.com>
2020-06-21 11:51:58 -0400

    FINAL VERSION FOR version 1 HOPE FULLY WELL IM GONNA ARCHIVE AFTER THIS AND TEST TO MAKE SURE ADS WORK. Ads weren't working well last time, so i came in here, made some changes to add a background color to the tabview and i am pretty sure that the banner ads aren't showing up because i only recently added it last night and it takes a while to work.

--- Refrigerator/Views/TabBarView.swift
+++ Refrigerator/Views/TabBarView.swift
@@ -32,7 +32,7 @@ struct TabBarView: View {
     }
     var body: some View {
         ZStack{
-            
+            Color.white
         TabView(selection: $selection){
             
             HomeView()
