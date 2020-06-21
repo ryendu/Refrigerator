eb62d69166b93acfac1a1186460ec1e2b00a9037
Ryan <ryandu9221@gmail.com>
2020-06-21 11:07:53 -0400

    just a little fix

--- Refrigerator/System Delegates/AppDelegate.swift
+++ Refrigerator/System Delegates/AppDelegate.swift
@@ -44,6 +44,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         
+        
+    
+        
         UserDefaults.standard.set(false, forKey: "RefrigeratorViewLoadedAd")
         UserDefaults.standard.set(false, forKey: "IndivisualRefrigeratorViewLoadedAd")
         UserDefaults.standard.set(false, forKey: "ExamineRecieptViewLoadedAd")
