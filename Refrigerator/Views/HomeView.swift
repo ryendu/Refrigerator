7acfbdd9844bba2c8fe131b312ebca2e0cd4725a
Ryan <ryandu9221@gmail.com>
2020-06-24 17:51:13 -0400

    attempt fixed ads issue

--- Refrigerator/Views/HomeView.swift
+++ Refrigerator/Views/HomeView.swift
@@ -67,15 +67,18 @@ struct HomeView: View {
                         VStack {
                             
                             VStack{
-                                HStack{
-                                    Text("Here's a fun fact")
-                                        .font(.custom("SF Compact Display", size: 23))
-                                        .padding()
-                                    
-                                    Spacer()
-                                }
-                                
+                                if self.funFactText != "" {
+                                    HStack{
+                                        Text("Here's a fun fact")
+                                            .font(.custom("SF Compact Display", size: 23))
+                                            .padding()
+                                        
+                                        Spacer()
+                                    }
                                 FunFactCell(funFact: self.funFactText, geo: geo)
+                                }else {
+                                    
+                                }
                             }.multilineTextAlignment(.center)
                             
                             if self.foodItem.count >= 10{
